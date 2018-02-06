6d50cb223f591a6a75b51e4855458d0d90f216e8
gregor slana <slgrega@yahoo.com>
2018-02-06 20:02:08 +0100

    bbb

---
+++ CRUDBasicsWebApi/Controllers/AspNetUsersController.cs
@@ -0,0 +1,19 @@
+﻿using CRUDBasicsWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDBasicsWebApi.Controllers
+{
+    public class AspNetUsersController : ApiController
+    {
+		private ApplicationDbContext Context = new ApplicationDbContext();
+		public IHttpActionResult Get() {
+			var retval = Context.Users.Select(x => new { Id = x.Id, Email = x.Email });
+			return Ok(retval);
+		}
+    }
+}
